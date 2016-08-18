133c02ce38d6cef0cd58fe227ad817e0b58035b9
Kevin Traw <ktraw2@gmail.com>
2016-08-18 15:54:02 -0700

    Added conversion function and began emoji mapping.

--- app/src/main/java/com/ksoft/emojiconverter/MainActivity.java
+++ app/src/main/java/com/ksoft/emojiconverter/MainActivity.java
@@ -1,15 +1,36 @@
 package com.ksoft.emojiconverter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<MappedEmoji> mappedEmojis = new ArrayList<MappedEmoji>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //map all emojis
+        mappedEmojis.add(new MappedEmoji("0x1F600", true));
+        mappedEmojis.add(new MappedEmoji("0x1F601", true));
+        mappedEmojis.add(new MappedEmoji("0x1F602", true));
+        mappedEmojis.add(new MappedEmoji("0x1F923", true));
+        mappedEmojis.add(new MappedEmoji("0x1F604", true));
+        mappedEmojis.add(new MappedEmoji("0x1F605", true));
+        mappedEmojis.add(new MappedEmoji("0x1F606", true));
+        mappedEmojis.add(new MappedEmoji("0x1F609", true));
+        mappedEmojis.add(new MappedEmoji("0x1F60A", true));
+        mappedEmojis.add(new MappedEmoji("0x1F60B", true));
+        mappedEmojis.add(new MappedEmoji("0x1F60E", true));
+        mappedEmojis.add(new MappedEmoji("0x1F60D", true));
     }
 
     /**
@@ -17,6 +38,33 @@ public class MainActivity extends AppCompatActivity {
      * @param view the current view
      */
     public void convertEmoji(View view) {
-        
+        ImageView img = (ImageView) findViewById(R.id.image);
+        img.setImageResource(R.drawable.websitefavicon);
+        //TextView txt = (TextView) findViewById(R.id.prompt);
+        EditText edit = (EditText) findViewById(R.id.edit);
+        //txt.setText("hello " + new String(Character.toChars(0x1F600)) + " " + edit.getText() + " " + edit.getText().toString().codePointAt(0));
+        if (Integer.parseInt(Integer.toHexString(edit.getText().toString().codePointAt(0)), 16) == 0x1F600)
+            System.out.println("you're a good programmer!");
+        else
+            System.out.println("not equal");
+        String codepointEntered = "0x" + Integer.toHexString(edit.getText().toString().codePointAt(0)).toUpperCase();
+        System.out.println("HEX CODE POINT: " + codepointEntered);
+        boolean valid = false;
+        MappedEmoji validEmoji = null;
+        for (MappedEmoji m : mappedEmojis)
+            if (codepointEntered.equals(m.getUnicodeHexString()))
+            {
+                valid = true;
+                validEmoji = m;
+            }
+        if (valid && validEmoji != null)
+        {
+            System.out.println("gooood programmer!");
+            System.out.println(validEmoji.getResName());
+            int resID = getResources().getIdentifier(validEmoji.getResName(), "drawable", getPackageName());
+            System.out.println("resID: " + resID);
+            img.setImageResource(resID);
+        }
+
     }
 }
--- app/src/main/java/com/ksoft/emojiconverter/MappedEmoji.java
+++ app/src/main/java/com/ksoft/emojiconverter/MappedEmoji.java
@@ -1,21 +1,36 @@
 package com.ksoft.emojiconverter;
 
+import android.graphics.drawable.Drawable;
+import android.util.StringBuilderPrinter;
+
 /**
  * Created by Kevin on 8/15/2016.
  */
 public class MappedEmoji {
-    private char unicodeID;
+    private String unicodeHexString;
     private String resName;
 
-    public MappedEmoji(char charID, String resource)
+    public MappedEmoji(String codepoint, String resource)
     {
-        unicodeID = charID;
+        unicodeHexString = codepoint;
         resName = resource;
     }
 
-    public char getChar()
+    public MappedEmoji(String codepoint, boolean convert0xtouUnderscore)
     {
-        return unicodeID;
+        unicodeHexString = codepoint;
+        if (convert0xtouUnderscore)
+        {
+            resName = "u_" + (codepoint).substring(2).toLowerCase();
+            System.out.println(resName);
+        }
+        else
+            resName = codepoint + ".png";
+    }
+
+    public String getUnicodeHexString()
+    {
+        return unicodeHexString;
     }
 
     public String getResName()
