a1537aa13fe6176328b4134c7144b659c3c57434
Kevin Traw <ktraw2@gmail.com>
2016-08-19 11:14:15 -0700

    Added a programmatic method of mapping emojis

--- app/src/main/java/com/ksoft/emojiconverter/MainActivity.java
+++ app/src/main/java/com/ksoft/emojiconverter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,75 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //map all emojis
-        mappedEmojis.add(new MappedEmoji("0x1F600", true));
-        mappedEmojis.add(new MappedEmoji("0x1F601", true));
-        mappedEmojis.add(new MappedEmoji("0x1F602", true));
-        mappedEmojis.add(new MappedEmoji("0x1F923", true));
-        mappedEmojis.add(new MappedEmoji("0x1F604", true));
-        mappedEmojis.add(new MappedEmoji("0x1F605", true));
-        mappedEmojis.add(new MappedEmoji("0x1F606", true));
-        mappedEmojis.add(new MappedEmoji("0x1F609", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60A", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60B", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60E", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60D", true));
-        mappedEmojis.add(new MappedEmoji("0x1F618", true));
-        mappedEmojis.add(new MappedEmoji("0x1F617", true));
-        mappedEmojis.add(new MappedEmoji("0x1F619", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61A", true));
-        mappedEmojis.add(new MappedEmoji("0x263A", true));
-        mappedEmojis.add(new MappedEmoji("0x1F642", true));
-        mappedEmojis.add(new MappedEmoji("0x1F917", true));
-        mappedEmojis.add(new MappedEmoji("0x1F914", true));
-        mappedEmojis.add(new MappedEmoji("0x1F610", true));
-        mappedEmojis.add(new MappedEmoji("0x1F611", true));
-        mappedEmojis.add(new MappedEmoji("0x1F636", true));
-        mappedEmojis.add(new MappedEmoji("0x1F644", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60F", true));
-        mappedEmojis.add(new MappedEmoji("0x1F623", true));
-        mappedEmojis.add(new MappedEmoji("0x1F625", true));
-        mappedEmojis.add(new MappedEmoji("0x1F62E", true));
-        mappedEmojis.add(new MappedEmoji("0x1F910", true));
-        mappedEmojis.add(new MappedEmoji("0x1F62F", true));
-        mappedEmojis.add(new MappedEmoji("0x1F62A", true));
-        mappedEmojis.add(new MappedEmoji("0x1F62B", true));
-        mappedEmojis.add(new MappedEmoji("0x1F634", true));
-        mappedEmojis.add(new MappedEmoji("0x1F60C", true));
-        mappedEmojis.add(new MappedEmoji("0x1F913", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61B", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61C", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61D", true));
-        mappedEmojis.add(new MappedEmoji("0x1F612", true));
-        mappedEmojis.add(new MappedEmoji("0x1F613", true));
-        mappedEmojis.add(new MappedEmoji("0x1F614", true));
-        mappedEmojis.add(new MappedEmoji("0x1F615", true));
-        mappedEmojis.add(new MappedEmoji("0x1F643", true));
-        mappedEmojis.add(new MappedEmoji("0x1F911", true));
-        mappedEmojis.add(new MappedEmoji("0x1F632", true));
-        mappedEmojis.add(new MappedEmoji("0x2639", true));
-        mappedEmojis.add(new MappedEmoji("0x1F641", true));
-        mappedEmojis.add(new MappedEmoji("0x1F616", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61E", true));
-        mappedEmojis.add(new MappedEmoji("0x1F61F", true));
-        mappedEmojis.add(new MappedEmoji("0x1F624", true));
-        mappedEmojis.add(new MappedEmoji("0x1F622", true));
-        mappedEmojis.add(new MappedEmoji("0x1F62D", true));
-        mappedEmojis.add(new MappedEmoji("0x1F626", true));
-        mappedEmojis.add(new MappedEmoji("0x1F627", true));
-        mappedEmojis.add(new MappedEmoji("0x1F628", true));
-        mappedEmojis.add(new MappedEmoji("0x1F629", true));
-        mappedEmojis.add(new MappedEmoji("0x1F630", true));
-        mappedEmojis.add(new MappedEmoji("0x1F631", true));
-        mappedEmojis.add(new MappedEmoji("0x1F633", true));
-        mappedEmojis.add(new MappedEmoji("0x1F635", true));
-        mappedEmojis.add(new MappedEmoji("0x1F621", true));
-        mappedEmojis.add(new MappedEmoji("0x1F620", true));
-        mappedEmojis.add(new MappedEmoji("0x1F607", true));
-        mappedEmojis.add(new MappedEmoji("0x1F637", true));
-        mappedEmojis.add(new MappedEmoji("0x1F912", true));
-        mappedEmojis.add(new MappedEmoji("0x1F915", true));
-        mappedEmojis.add(new MappedEmoji("0x1F608", true));
-        mappedEmojis.add(new MappedEmoji("0x1F47F", true));
+        for (Field field : R.drawable.class.getFields())
+        {
+            String name = field.getName();
+            if (!(name.equals("unknown")))
+                mappedEmojis.add(new MappedEmoji("0x" + name.substring(2).toUpperCase(), true));
+        }
     }
 
     /**
@@ -119,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 {
                     valid = true;
                     validEmoji = m;
+                    break;
                 }
             if (valid && validEmoji != null)
             {
